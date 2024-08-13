ce09356120a30d6e1eb5ce4dc9e2b00af0fa9518
tjsdn3031 <tjsdn3031@gmail.com>
2024-08-13 11:18:29 +0900

    20240813

---
+++ src/com/mycompany/ch10/first/TryCatchFinallEx.java
@@ -0,0 +1,20 @@
+package com.mycompany.ch10.first;
+
+public class TryCatchFinallEx {
+
+	public static void main(String[] args) {
+		
+		try {
+			Class clazzz = Class.forName("ExTry");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+			
+		
+		
+		
+	}
+
+}
---
+++ src/com/mycompany/ch10/second/ThrowsEx.java
@@ -0,0 +1,11 @@
+package com.mycompany.ch10.second;
+
+public class ThrowsEx {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		Class clazzz = Class.forName("ExTry");
+
+	}
+
+}
